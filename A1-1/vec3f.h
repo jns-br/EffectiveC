d46b1e136f6f06335cb026e96a9497929688f819
jba <jonas.bauer@fokus.fraunhofer.de>
2020-04-25 13:02:25 +0200

    implement basic vec3 headder

--- A1-1/vec3f.h
+++ A1-1/vec3f.h
@@ -1,5 +1,15 @@
+#include <array>
+
 namespace my {
     class Vec3f {
+        public:
+            Vec3f(float x, float y, float z);
+
+            std::array<float, 3> vec3f() const;
+            void set_vec3f(std::array<float, 3> vec3f);
+        
+        private:
+            std::array<float, 3> vec3f_;
 
     };
 }
