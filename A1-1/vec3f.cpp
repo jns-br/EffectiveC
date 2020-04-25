d0403ddbd2c2e61a1eb1cc9ef84c916a03f752a0
jba <jonas.bauer@fokus.fraunhofer.de>
2020-04-25 13:23:42 +0200

    implement basic vec3f cpp

---
+++ A1-1/vec3f.cpp
@@ -0,0 +1,19 @@
+#include "vec3f.h"
+
+using namespace my;
+
+Vec3f::Vec3f(float x, float y, float z)
+    : vec3f_({x, y, z})
+{}
+
+std::array<float, 3>
+Vec3f::vec3f() const
+{
+    return vec3f_;
+}
+
+void
+Vec3f::set_vec3f(std::array<float, 3> vec3f)
+{
+    vec3f_ = vec3f;
+}
