9d0a759843d67a7c55bb8bf4764baa2a3593fbbd
jba <jonas.bauer@fokus.fraunhofer.de>
2020-04-25 13:45:43 +0200

    implement default constructor

--- A1-1/vec3f.h
+++ A1-1/vec3f.h
@@ -5,7 +5,7 @@
 namespace my {
     class Vec3f {
         public:
-            Vec3f(float x, float y, float z);
+            Vec3f(float x=0, float y=0, float z=0);
 
             std::array<float, 3> vec3f() const;
             void set_vec3f(std::array<float, 3> vec3f);
