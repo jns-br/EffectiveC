6cb5ba3fe83a2f9667e67ae22e720ca890fe38d0
jba <jonas.bauer@fokus.fraunhofer.de>
2020-05-25 11:29:49 +0200

    implement all constructors

--- A2-1/my_vector.h
+++ A2-1/my_vector.h
@@ -9,22 +9,42 @@ namespace my {
         public:
             vector();
             vector(const size_t& n);
+            vector(const size_t& n, const T& val);
             ~vector();
+            bool empty() const;
         private:
             T* data_;
+            size_t size_;
+            size_t capacity_;
 
     };
     
     template<typename T>
-    vector<T>::vector() {}
+    vector<T>::vector() : data_(new T[0]), size_(0), capacity_(0) {}
 
     template<typename T>
-    vector<T>::vector(const size_t& n) : data_(new T[n]) {} 
+    vector<T>::vector(const size_t& n) : data_(new T[n]), size_(0), capacity_(n)  {}
+
+    template<typename T>
+    vector<T>::vector(const size_t& n, const T& val) : data_(new T[n]), size_(n), capacity_(n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            this->data_[i] = val;
+        }
+        
+    } 
 
     template<typename T>
     vector<T>::~vector()
     {
         delete[] data_;
+    }
+
+    template<typename T>
+    bool vector<T>::empty() const
+    {
+
     }
 
 }
