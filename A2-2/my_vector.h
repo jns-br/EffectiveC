320401cce1eb1ff35440b314da88eadad1b842ee
jba <jonas.bauer@fokus.fraunhofer.de>
2020-06-05 09:28:32 +0200

    add capacity member variable

--- A2-2/my_vector.h
+++ A2-2/my_vector.h
@@ -27,16 +27,17 @@ namespace my {
         private:
             T* data_;
             size_t size_;
+            size_t capacity_;
     };
     
     template<typename T>
-    vector<T>::vector() : data_(new T[0]), size_(0) {}
+    vector<T>::vector() : data_(new T[0]), size_(0), capacity_(0) {}
 
     template<typename T>
-    vector<T>::vector(const size_t& n) : data_(new T[n]), size_(0)  {}
+    vector<T>::vector(const size_t& n) : data_(new T[n]), size_(0), capacity_(n)  {}
 
     template<typename T>
-    vector<T>::vector(const size_t& n, const T& val) : data_(new T[n]), size_(n)
+    vector<T>::vector(const size_t& n, const T& val) : data_(new T[n]), size_(n), capacity_(n)
     {
         for (int i = 0; i < n; i++)
         {
