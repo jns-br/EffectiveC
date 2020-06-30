30706177af8a8bd05134f67db694fa8a4aa75da3
jba <jonas.bauer@fokus.fraunhofer.de>
2020-06-30 10:48:57 +0200

    start A3

---
+++ A3/main.cpp
@@ -0,0 +1,235 @@
+#include <iostream>
+#include <string>
+
+#include "assert.h"
+
+#include "treemap.h"
+#include "payload.h"
+
+using namespace std;
+using my::treemap;
+
+// helper function for human readable output
+string
+to_string(Payload p)
+{
+    return string("("+to_string(p.x)+","+to_string(p.y)+","+to_string(p.z)+")");
+}
+
+int main()
+{
+
+    {
+        cout << "1. treemap basics..." << endl;
+
+        treemap<string, Payload> m;
+        assert(m.size() == 0);
+        assert(m.count("A") == 0);
+        assert(Payload::count() == 0);
+
+        cout << "done..." << endl;
+    }
+
+    assert(Payload::count() == 0);
+    cout << endl;
+
+    {
+        cout << "2. insert, find, count, size, clear ..." << endl;
+
+        // insert()
+        treemap<string, Payload> m;
+        m.insert("Three", Payload(3,3,3));
+        m.insert("Seven", Payload(7,7,7));
+        auto [iter_eleven, was_inserted] = m.insert("Eleven", Payload(11,11,11));
+
+        assert(was_inserted == true);
+        assert(iter_eleven->first == "Eleven");
+        assert(iter_eleven->second == Payload(11,11,11));
+        assert(m.size() == 3);
+        assert(m.count("Three") == 1);
+        assert(m.count("Four") == 0);
+        assert(Payload::count() == 3);
+
+        {
+
+            // find()
+            auto eleven = m.find("Eleven");
+            assert(Payload::count() == 3);
+
+            auto key = eleven->first;
+            auto value = eleven->second;
+            cout << "found payload Eleven: [ " << key << "," << to_string(value) << " ]" << endl;
+
+            // clear()
+            m.clear();
+            assert(m.size() == 0);
+            assert(Payload::count() == 1); // value contains one Payload obj
+        }
+        assert(Payload::count() == 0);
+
+        cout << "done." << endl;
+    }
+
+    assert(Payload::count() == 0);
+    cout << endl;
+
+    {
+        cout << "3. operator [] read/write..." << endl;
+
+        treemap<string, Payload> m;
+
+        // [] write operation
+        m["B=1"] = Payload(1,1,1);
+        m["A=2"] = Payload(2,2,2);
+        m["C=3"] = Payload(3,3,3);
+
+        // count()
+        assert(m.count("A=2") == 1);
+        assert(m.count("B=1") == 1);
+        assert(m.count("C=3") == 1);
+        assert(m.size() == 3);
+
+        // const version of operator[]
+        auto const& const_m = m;
+        cout << "A: " << to_string(const_m["A=2"]) << endl;
+        assert(const_m["A=2"] == Payload(2,2,2));
+        cout << "B: " << to_string(const_m["B=1"]) << endl;
+        assert(const_m["B=1"] == Payload(1,1,1));
+        cout << "C: " << to_string(const_m["C=3"]) << endl;
+        assert(const_m["C=3"] == Payload(3,3,3));
+
+        cout << "done." << endl;
+
+    }
+
+    assert(Payload::count() == 0);
+    cout << endl;
+
+    {
+        cout << "4. iterator, ++ ..." << endl;
+
+        treemap<string, int> m;
+
+        m["Hartmut"] = 1;
+        m["Helmut"] = 2;
+        m["Zebra"] = 3;
+        m["Anna"] = 4;
+        m["Klaus"] = 5;
+        assert(m.size() == 5);
+
+        // iterator++, iterator operator->
+        cout << "iterating through list in normal order:" << endl;
+        for(auto i=m.begin(); i!=m.end(); ++i)
+            cout << "  " << i->first << ": " << i->second << endl;
+
+        assert(m.begin()->first == "Anna");
+        assert((++m.begin())->first == "Hartmut");
+        assert((++++m.begin())->first == "Helmut");
+        assert((++++++m.begin())->first == "Klaus");
+        assert((++++++++m.begin())->first == "Zebra");
+
+        // iterator++, iterator operator*
+        cout << "iterating through list using range-based for loop:" << endl;
+        for(auto i : m)
+            cout << "  " << i.first << ": " << i.second << endl;
+
+        cout << "done." << endl;
+
+    }
+
+    assert(Payload::count() == 0);
+    cout << endl;
+
+    {
+        cout << "5. iterator, -- ..." << endl;
+
+        treemap<string, int> m;
+        m["Hartmut"] = 1;
+        m["Helmut"] = 2;
+        m["Zebra"] = 3;
+        m["Anna"] = 4;
+        m["Klaus"] = 5;
+        assert(m.size() == 5);
+
+        // iterator--
+        cout << "iterating through list in reverse order:" << endl;
+        /* todo */
+        // write this loop yourself; careful, one can easily screw up this test
+        // auto i = m.end();
+
+        // some bit of automatic testing in addition
+        assert((--m.end())->first == "Zebra");
+        assert((----m.end())->first == "Klaus");
+
+        cout << "done." << endl;
+
+    }
+
+    assert(Payload::count() == 0);
+    cout << endl;
+
+    {
+        cout << "6. move ..." << endl;
+        assert(Payload::count() == 0); // just for sanity's sake
+
+        treemap<string, Payload> m;
+        m["Hartmut"] = Payload(1,1,1);
+        m["Helmut"]  = Payload(1,2,1);
+        m["Zebra"]   = Payload(1,3,1);
+        m["Anna"]    = Payload(1,4,1);
+        m["Klaus"]   = Payload(1,5,1);
+        assert(m.size() == 5);
+        assert(Payload::count() == 5);
+
+        // move it
+        auto m2 = std::move(m);
+
+        // no duplicates?
+        assert(Payload::count() == 5);
+
+        // keys and values still intact?
+        assert(m2["Anna"] == Payload(1,4,1));
+        assert(m2["Klaus"] == Payload(1,5,1));
+
+        cout << "done." << endl;
+    }        
+    assert(Payload::count() == 0);
+    cout << endl;
+
+    {
+        cout << "7. deep copy ..." << endl;
+
+        assert(Payload::count() == 0); // just for sanity's sake
+
+        treemap<string, Payload> m;
+        m["Hartmut"] = Payload(1,1,1);
+        m["Helmut"]  = Payload(1,2,1);
+        m["Zebra"]   = Payload(1,3,1);
+        m["Anna"]    = Payload(1,4,1);
+        m["Klaus"]   = Payload(1,5,1);
+        assert(m.size() == 5);
+        assert(Payload::count() == 5);
+
+        // copy it
+        auto m2 = m;
+
+        // exactly twice as many objects now?
+        assert(Payload::count() == 10);
+
+        // keys and values still intact?
+        assert(m2["Anna"] == m["Anna"]);
+        assert(m2["Klaus"] == m["Klaus"]);
+
+        // two independent copies?
+        auto old_anna = m["Anna"];
+        m["Anna"] = Payload(99,100,101);
+        assert(m2["Anna"] == old_anna);
+
+        cout << "done." << endl;
+    }
+    assert(Payload::count() == 0);
+    cout << endl;
+
+
+    return 0;
+}
---
+++ A3/treemap.h
@@ -0,0 +1,291 @@
+#pragma once
+
+#include <memory>
+#include <iostream>
+#include <utility>
+#include <tuple>
+
+#include <cassert>
+
+// forward declaration to resolve cross-referencing
+namespace my {
+    template<typename K, typename T>
+    class treemap;
+}
+
+// forward declaration to resolve cross-referencing
+template<typename K, typename T>
+void
+swap(my::treemap<K,T>& lhs, my::treemap<K,T>& rhs);
+
+namespace my {
+
+/*
+ * class treemap<K,T>
+ * represents an associative container (dictionary) with unique keys
+ * implemented by a binary search tree
+ *
+ */
+template<typename K, typename T>
+class treemap
+{
+
+protected:
+
+    /*
+     * hierarchical node structure for class treemap
+     *
+     */
+    class node {
+
+    public:
+
+        // key and value represented as pair (so they can be easily passed along together)
+        std::pair<K, T> data;
+
+    }; // class node
+
+public:
+
+    // public type aliases
+    using key_type = K;
+    using mapped_type = T;
+    using value_type = std::pair<K, T>;
+
+    // iterator: references a node within the tree
+    class iterator {
+
+    protected:
+
+        // treemap is a friend, can call protected constructor
+        friend class treemap;
+
+        // construct iterator referencing a speciic node
+        // - only treemap shall be allowed to do so
+        iterator(/* todo */)
+        {}
+
+        // non-owning reference to the actual node
+        /* todo */
+
+    public:
+
+        // access data of referenced map element (node)
+        value_type& operator*()
+        {
+            /* todo */ static value_type dummy; return dummy;
+        }
+        value_type* operator->()
+        {
+            /* todo */ static value_type dummy; return &dummy;
+        }
+
+        // two iterators are equal if they point to the same node
+        bool operator==(const iterator&) const
+        {
+            /* todo */ return false;
+        }
+
+        bool operator!=(const iterator&) const
+        {
+            /* todo */ return false;
+        }
+
+        // next element in map, pre-increment
+        // note: must modify self!
+        iterator& operator++()
+        {
+            /* todo */ static iterator dummy; return dummy;
+        }
+
+        // prev element in map, pre-decrement
+        // note: must modify self!
+        iterator& operator--()
+        {
+            /* todo */ static iterator dummy; return dummy;
+        }
+
+    }; // class iterator
+
+
+    // used for copy&move
+    template<typename KK, typename TT>
+    friend void ::swap(treemap<KK,TT>& , treemap<KK,TT>& );
+
+    // construct empty map
+    treemap();
+
+    // move ctor
+    treemap(treemap<K,T>&&);
+
+    // deep copy ctor
+    treemap(const treemap<K,T>&);
+
+    // how often is the element contained in the map?
+    // (for this type of container, can only return 0 or 1)
+    size_t count(const K&) const;
+
+    // assignment (move & copy)
+    treemap<K,T>& operator=(treemap<K,T>);
+
+    // remove/destroy all elements
+    void clear();
+
+    // random read-only access to value by key, does not modify map
+    T operator[](const K& ) const;
+
+    // random write access to value by key
+    T& operator[](const K&);
+
+    // number of elements in map (nodes in tree)
+    size_t size() const;
+
+    // iterator referencing first element (node) in map
+    iterator begin();
+
+    // iterator referencing no element (node) in map
+    iterator end() const;
+
+    // add a new element into the tree
+    // returns pair, consisting of:
+    // - iterator to element
+    // - bool
+    //   - true if element was inserted;
+    //   - false if key was already in map (will not overwrite existing value)
+    std::pair<iterator,bool> insert(const K&, const T&);
+
+    // add a new element into the tree, or overwrite existing element if key already in map
+    // returns:
+    // - iterator to element
+    // - true if element was newly created; false if existing element was overwritten
+    std::pair<iterator,bool> insert_or_assign(const K&, const T&);
+
+    // find element with specific key. returns end() if not found.
+    iterator find(const K&) const;
+
+};
+
+template<typename K, typename T>
+treemap<K,T>::treemap()
+{
+}
+
+template<typename K, typename T>
+void
+treemap<K,T>::clear()
+{
+}
+
+// random read-only access to value by key
+template<typename K, typename T>
+T
+treemap<K,T>::operator[](const K&) const
+{
+    /* todo */ return T();
+}
+
+// random write access to value by key
+template<typename K, typename T>
+T&
+treemap<K,T>::operator[](const K&)
+{
+    /* todo */ static T dummy; return dummy;
+}
+
+// number of elements in map (nodes in tree)
+template<typename K, typename T>
+size_t
+treemap<K,T>::size() const
+{
+    /* todo */ return 666;
+}
+
+// move ctor
+template<typename K, typename T>
+treemap<K,T>::treemap(treemap<K,T>&&)
+{
+    /* todo */ 
+}
+
+// deep copy ctor
+template<typename K, typename T>
+treemap<K,T>::treemap(const treemap<K,T>&)
+{
+    /* todo */ 
+}
+
+// assignment (move & copy)
+template<typename K, typename T>
+treemap<K,T>& 
+treemap<K,T>::operator=(treemap<K,T>)
+{
+    /* todo, use copy&swap */ 
+    return *this;
+}
+
+
+// iterator referencing first element (node) in map
+template<typename K, typename T>
+typename treemap<K,T>::iterator
+treemap<K,T>::begin()
+{
+    /* todo */ return iterator();
+}
+
+// iterator referencing no element (node) in map
+template<typename K, typename T>
+typename treemap<K,T>::iterator
+treemap<K,T>::end() const
+{
+    /* todo */ return iterator();
+}
+
+// add a new element into the tree
+// returns:
+// - iterator to element
+// - true if element was inserted; false if key was already in map
+template<typename K, typename T>
+std::pair<typename treemap<K,T>::iterator,bool>
+treemap<K,T>::insert(const K&, const T&)
+{
+    /* todo */ return std::make_pair(iterator(),false);
+}
+
+// add a new element into the tree, or overwrite existing element if key already in map
+// returns:
+// - iterator to element
+// - true if element was newly created; false if existing element was overwritten
+template<typename K, typename T>
+std::pair<typename treemap<K,T>::iterator,bool>
+treemap<K,T>::insert_or_assign(const K&, const T&)
+{
+    /* todo */ return std::make_shared(iterator(),false);
+}
+
+// find element with specific key. returns end() if not found.
+template<typename K, typename T>
+typename treemap<K,T>::iterator
+treemap<K,T>::find(const K&) const
+{
+    /* todo */ return iterator();
+}
+
+// how often is the element contained in the map?
+template<typename K, typename T>
+size_t
+treemap<K,T>::count(const K&) const
+{
+    /* todo */ return 666;
+}
+
+} // namespace my
+
+// swap is overloaded in global namespace
+// see https://stackoverflow.com/questions/11562/how-to-overload-stdswap
+// (answer by Attention Mozza314)
+template<typename K, typename T>
+void
+swap(my::treemap<K,T>&, my::treemap<K,T>&)
+{
+}
+
