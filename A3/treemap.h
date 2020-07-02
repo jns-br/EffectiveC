73024a0015e5fe730c46dfec17a770bdf8f3d511
jba <jonas.bauer@fokus.fraunhofer.de>
2020-07-02 11:09:31 +0200

    fix operator-> of iterator

--- A3/treemap.h
+++ A3/treemap.h
@@ -158,7 +158,7 @@ namespace my {
                     }
                     value_type* operator->()
                     {
-                        auto node_ptr = nodeObserver_.lock();
+                        static auto node_ptr = nodeObserver_.lock();
                         return &node_ptr->data_;
                     }
 
